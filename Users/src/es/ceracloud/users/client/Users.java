853b8f8afa3b2b23223a75b622ac7b3f03aba930
Tadeo Julián Segarra <tjulian@ceracloud.es>
2015-09-30 01:47:43 +0200

    Falla la comunicación a traves del request factory.

--- Users/src/es/ceracloud/users/client/Users.java
+++ Users/src/es/ceracloud/users/client/Users.java
@@ -1,11 +1,39 @@
 package es.ceracloud.users.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.web.bindery.event.shared.EventBus;
+import com.google.web.bindery.event.shared.SimpleEventBus;
+import com.google.web.bindery.requestfactory.shared.Receiver;
+import com.google.web.bindery.requestfactory.shared.Request;
+
+import es.ceracloud.users.client.UserRequestFactory.UserRequestContext;
 
 public class Users implements EntryPoint {
 
+	private UserRequestFactory requestFactory;
+	
 	@Override
 	public void onModuleLoad() {
+		final EventBus eventBus = new SimpleEventBus();
+		requestFactory = GWT.create(UserRequestFactory.class);
+		requestFactory.initialize(eventBus);
+		
+		UserRequestContext request = requestFactory.context();
+		UserProxy user = request.create(UserProxy.class);
+		user.setName("Tadeo");
+		user.setEmail("tadeo.julian@gmail.com");
+		
+		Request<UserProxy> createRequest = request.findByID((long)1);
+		
+		createRequest.fire(new Receiver<UserProxy>() {
+
+			@Override
+			public void onSuccess(UserProxy response) {
+				System.out.println(response.getName());
+			}
+			
+		});
 		
 	}
 	
--- Users/src/es/ceracloud/users/server/internal/UsersDaoImpl.java
+++ Users/src/es/ceracloud/users/server/internal/UsersDaoImpl.java
@@ -7,12 +7,18 @@ public class UsersDaoImpl implements UsersDao {
 
 	@Override
 	public User findByID(Long id) {
+		if (id == 1) {
+			User user = new User();
+			user.setId((long) 1);
+			user.setName("Tadeo");
+			user.setEmail("tadeo.julian@gmail.com");
+		}
 		return null;
 	}
 
 	@Override
 	public void save(User user) {
-		
+
 	}
 
 }
